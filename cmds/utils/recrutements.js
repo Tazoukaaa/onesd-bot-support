6849360be8a015eb397efcdb5ca5b16e35737d7e
Tazoukaa <78051147+Tazoukaaa@users.noreply.github.com>
2021-01-27 12:43:41 +0100

    Update recrutements.js

--- cmds/utils/recrutements.js
+++ cmds/utils/recrutements.js
@@ -2,14 +2,13 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, message, args) => {
 
-    message.delete()
-
     const embed = new Discord.MessageEmbed()
+    message.delete()
     .setDescription("**Bonjour, pour les recrutements, rendez vous dans le salon spécifique à cela !**")
     .setColor("0000FF")
-    message.channel.send({ embed });
-    };
+    message.channel.send(embed)
+};
 
- module.exports.config = {
+module.exports.config = {
      name: "rc"
- }
+}
